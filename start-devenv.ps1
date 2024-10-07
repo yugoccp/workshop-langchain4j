0c4c179797dabaf0974b184d993e38c4effcab25
yugoccp <yugoccp@gmail.com>
2024-10-07 00:36:54 -0300

    fix: reverted docker-compose.yml for better dev experience

--- start-devenv.ps1
+++
@@ -1,3 +0,0 @@
-docker build -t localhost/workshop-devenv  .
-docker run -p 8080:8080 -v ./src:/workspace/src --name workshop-devenv --rm localhost/workshop-devenv
-docker exec -it workshop-devenv bash
--- start-devenv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t localhost/workshop-devenv .
-docker run -p 8080:8080 -v ./src:/workspace/src --name workshop-devenv --rm localhost/workshop-devenv
-docker exec -it workshop-devenv bash
