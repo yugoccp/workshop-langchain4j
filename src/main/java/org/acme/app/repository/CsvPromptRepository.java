105fc7406159642b48b08ae7cb5a96ef631fd620
yugoccp <yugoccp@gmail.com>
2025-02-10 09:00:33 -0300

    feat: add log after reading csv file

--- src/main/java/org/acme/app/repository/CsvPromptRepository.java
+++ src/main/java/org/acme/app/repository/CsvPromptRepository.java
@@ -13,6 +13,8 @@ import java.util.stream.Stream;
 @ApplicationScoped
 public class CsvPromptRepository implements PromptRepository {
 
+    private static final Logger logger = LoggerFactory.getLogger(CsvPromptRepository.class);
+
     private static final String CSV_DELIMITER = ";";
     private static final String DEFAULT_PROMPT_FILE = "prompts.csv";
 
@@ -33,6 +35,7 @@ public class CsvPromptRepository implements PromptRepository {
 
     private List<String[]> readCsv(String filename) {
         var csvFile = DocumentLoader.getResourceDocument(filename).text();
+        logger.debug("Successfully read file: {}", filename);
         return Stream.of(csvFile.split("\n"))
                 .map(line -> line.split(CSV_DELIMITER))
                 .toList();
